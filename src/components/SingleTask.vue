58d443d224b7340ad682d194d05ad80183591568
ahmed-b20 <ahmedbanawiwork@gmail.com>
2022-10-26 14:24:40 +0200

    work on task description in my day tap

--- src/components/SingleTask.vue
+++ src/components/SingleTask.vue
@@ -12,7 +12,60 @@
                     {{ task.name }}
                 </span>
 
-                <TaskInfoIconsVue :index='index' :task="task" />
+                <span :data-id="index" class="info-icons">
+                    <span :data-id="index" v-if="task.steps.length > 0" class="info-icon steps">
+                        <img :data-id="index" src="@/assets/design-material/icons/process.png" alt="task steps"
+                            title="task steps">
+                        {{ (task.steps.filter((step) => { return step.complete === true }).length) }} Of {{
+                                task.steps.length
+                        }}
+                    </span>
+
+                    <!-- <span :data-id="index" class="border" v-if="task.steps.length > 0 && task.note"></span> -->
+
+                    <span :data-id="index" class="info-icon note" v-if="task.note">
+                        <img :data-id="index" src="@/assets/design-material/icons/notes.png" alt="task notes"
+                            title="task notes">Note
+                    </span>
+
+                    <!-- <span :data-id="index" class="border"
+                        v-if="task.steps.length > 0 && task.dueDateName || task.note && task.dueDateName"></span> -->
+
+                    <span
+                        :class="{ delete: new Date() > new Date(new Date(task.dueTime).setDate(new Date(task.dueTime).getDate() + 1)) && !task.complete }"
+                        :data-id="index" class="info-icon note" v-if="task.dueDateName">
+                        <img :data-id="index" src="@/assets/design-material/icons/due-date.png" :alt="task.dueDateName"
+                            title="task notes">
+                        {{ task.dueDateName }}
+                    </span>
+
+                    <!-- <span :data-id="index" class="border"
+                        v-if="task.steps.length > 0 && task.addToMyDay || task.note && task.addToMyDay || task.dueDateName && task.addToMyDay"></span> -->
+
+                    <span :data-id="index" class="info-icon add-task-container note" v-if="task.addToMyDay">
+                        <img :data-id="index" src="@/assets/design-material/icons/my-day.png" :alt="task.addToMyDay"
+                            title="task notes">
+                        My Day
+                    </span>
+
+                    <!-- <span :data-id="index" class="border"
+                        v-if="task.steps.length > 0 && task.addToMyDay || task.note && task.addToMyDay || task.dueDateName && task.addToMyDay|| task.dueDateName && !!task.repeatDueDateName"></span> -->
+
+                    <span :data-id="index" class="info-icon note" v-if="!!task.repeatDueDateName">
+                        <img :data-id="index" src="@/assets/design-material/icons/repeat.png"
+                            :alt="task.repeatDueDateName" title="task notes">
+                        {{ task.repeatDueDateName }}
+                    </span>
+
+                    <!-- <span :data-id="index" class="border"
+                        v-if="task.steps.length > 0 && task.addToMyDay || task.note && task.addToMyDay || task.dueDateName && task.addToMyDay|| task.dueDateName && !!task.repeatDueDateName|| task.repeatDueDateName && !!task.remindMe"></span> -->
+
+                    <span :data-id="index" class="info-icon note" v-if="!!task.remindMe">
+                        <img :data-id="index" src="@/assets/design-material/icons/reminder.png" :alt="task.remindMe"
+                            title="task notes">
+                        {{ task.remindMe }}
+                    </span>
+                </span>
             </span>
 
             <img v-if="task.important" :data-id="index" @click="importantToggle('task')" class="important-toggle"
@@ -159,16 +212,13 @@ import { allLists } from '@/stores/allLists.js'
 import { mapState, mapWritableState } from 'pinia'
 import PopUp from './PopUp.vue'
 import DropDown from '../components/DropDown.vue';
-import TaskInfoIconsVue from './task/TaskInfoIcons.vue';
-
 export default {
     name: 'SingleTask',
     props: ['listId', 'toggleShrink', 'childId', 'chosenSmartList'],
     emits: ['openDescriptionEvent'],
     components: {
         PopUp,
-        DropDown,
-        TaskInfoIconsVue
+        DropDown
     },
     beforeMount() {
         this.lists.forEach((list, index) => {
@@ -279,7 +329,7 @@ export default {
                     return true
                 }
             }
-        },
+        }
     },
     watch: {
         toggleShrink() {
