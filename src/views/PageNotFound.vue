763d856fc4bf8676f7d286bc871e4df97a71aefc
ahmed-b20 <ahmedbanawiwork@gmail.com>
2022-11-13 17:02:36 +0200

    finish not found page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,13 +1,35 @@
 <template>
-    <h1>Page Not Found</h1>
+    <div class="view-content-container">
+        <div class="title">
+            <div class="toggle">
+                <img @click.self="toggleAside" src="@/assets/design-material/icons/menu.png" alt="open-sidebar">
+            </div>
+        </div>
+
+        <div class="page-not-found">
+            <h1 class="no-result">No Results Found</h1>
+        </div>
+    </div>
 </template>
 
 <script>
+import { mapState, mapWritableState } from 'pinia'
+import { toggleAside } from '@/stores/toggleAside.js'
+
+
 export default {
-    name:'PageNotFound',
+    name: 'PageNotFound',
     data() {
         return {
-            
+
+        }
+    },
+    computed: {
+        ...mapWritableState(toggleAside, ['toggleState']),
+    },
+    methods: {
+        toggleAside() {
+            this.toggleState = !this.toggleState
         }
     },
 }
