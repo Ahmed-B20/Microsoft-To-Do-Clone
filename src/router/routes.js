8e4e3b343e5ee28c4b7db57eb9729516791f2443
ahmed-b20 <ahmedbanawiwork@gmail.com>
2022-11-15 09:26:18 +0200

    start work in login bage

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import List from "@/views/List.vue";
 import PageNotFound from "@/views/PageNotFound.vue";
+import Login from "@/views/Login.vue";
 
 const routes = [
   { path: "/",name:'home', redirect: '/list/0'  },
   { path: "/list",name:'homePage', redirect: '/list/0'  },
+  { path: "/login",name:'login', component: Login  },
   { path: "/list/:listId(\\d+)", name:'list', component: List, props:true },
   { path: "/list/:listId(\\d+)/:childId(\\d+)", name:'child-list', component: List, props:true },
   { path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFound },
