3c0f706e09ce5f90f515cd6fec49f65e2daabc9e
ahmed-b20 <ahmedbanawiwork@gmail.com>
2022-11-15 10:06:52 +0200

    start work in login bage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,48 @@
 <template>
     <teleport to='body'>
         <div class="login-container">
-            <div class="main">
-                <input type="checkbox" id="chk" aria-hidden="true">
-
-                <div class="signup">
-                    <form>
-                        <label for="chk" aria-hidden="true">Sign up</label>
-                        <input type="text" name="txt" placeholder="User name" required>
-                        <input type="email" name="email" placeholder="Email" required>
-                        <input type="password" name="pswd" placeholder="Password" required>
-                        <button>Sign up</button>
-                    </form>
+            <div class="main-content">
+                <div class="container">
+                    <div class="screen">
+                        <div class="screen__background">
+                            <span class="screen__background__shape screen__background__shape4"></span>
+                            <span class="screen__background__shape screen__background__shape3"></span>
+                            <span class="screen__background__shape screen__background__shape2"></span>
+                            <span class="screen__background__shape screen__background__shape1"></span>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="login">
-                    <form>
-                        <label for="chk" aria-hidden="true">Login</label>
-                        <input type="email" name="email" placeholder="Email" required>
-                        <input type="password" name="pswd" placeholder="Password" required>
-                        <button>Login</button>
-                    </form>
+                <div class="main">
+                    <input type="checkbox" id="chk" aria-hidden="true">
+
+                    <div class="signup">
+                        <form>
+                            <label for="chk" aria-hidden="true">Sign up</label>
+                            <input type="text" name="txt" placeholder="User name" required>
+                            <input type="email" name="email" placeholder="Email" required>
+                            <input type="password" name="pswd" placeholder="Password" required>
+                            <button>Sign up</button>
+                        </form>
+                    </div>
+
+                    <div class="login">
+                        <form>
+                            <label for="chk" aria-hidden="true">Login</label>
+                            <input type="email" name="email" placeholder="Email" required>
+                            <input type="password" name="pswd" placeholder="Password" required>
+                            <button>Login</button>
+                        </form>
+                    </div>
+
+                    <div class="screen">
+                        <div class="screen__background">
+                            <span class="screen__background__shape screen__background__shape4"></span>
+                            <span class="screen__background__shape screen__background__shape3"></span>
+                            <span class="screen__background__shape screen__background__shape2"></span>
+                            <span class="screen__background__shape screen__background__shape1"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
